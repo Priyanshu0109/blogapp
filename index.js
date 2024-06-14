d6a63a51b923e00b1f45d12b221c38f6668c55b8
PriyanshuKumar0109 <priyanshukumarsingh98266@gmail.com>
2024-06-14 13:22:47 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
+import dotenv from 'dotenv';
 //components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
